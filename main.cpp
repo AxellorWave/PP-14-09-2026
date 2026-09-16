22a299c28357b98fbf5f2a0178aede23dcc1a2d3
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-09-16 22:24:05 +0300

    calculate without threads

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include "clicker.hpp"
 
 using data_t = std::vector< unsigned long long >;
 using value_t = data_t::value_type;
@@ -10,5 +11,16 @@ int main()
   constexpr size_t size{1'000'000'000};
   double init{0}, total{0};
   value_t sum{0};
+  {
+    zharov::Clicker cl;
+    data_t values(size, 1);
+    init = cl.millisec();
+    for (size_t i = 0; i < values.size(); ++i)
+    {
+      sum+= values[i];
+    }
+    total = cl.millisec();
+  }
+  std::cout << total - init << "\n";
 }
 
