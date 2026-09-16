cec3a2b31580ed554d54000a9f60889c6a67906d
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-09-16 21:59:44 +0300

    init base values

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+
+using data_t = std::vector< unsigned long long >;
+using value_t = data_t::value_type;
+
 int main()
-{}
+{
+  constexpr size_t size{1'000'000'000};
+  double init{0}, total{0};
+  value_t sum{0};
+}
 
