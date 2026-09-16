6d642dbc58ba7ae5bdb84c8ae0952a0f65d48c01
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-09-16 23:42:16 +0300

    tester works

--- tester.cpp
+++ tester.cpp
@@ -1,4 +1,29 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <boost/process.hpp>
+#include <algorithm>
+
+namespace bp = boost::process;
 
 int main()
-{}
+{
+  std::cout << "threads time\n";
+  int threads = 2;
+  for (; threads <= 1024; threads*=2)
+  {
+    std::vector< double > v;
+    for (size_t i = 0; i < 5; ++i)
+    {
+      bp::ipstream out;
+      bp::child c("./counter", std::to_string(threads), bp::std_out > out);
+      double result;
+      out >> result;
+      c.wait();
+      v.push_back(result);
+    }
+    std::sort(v.begin(), v.end());
+    std::cout << threads << " " << v[2] << "\n";
+  }
+}
