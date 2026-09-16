eac2d3e87fd17d80158b3311d761d18aa3859ed6
AxellorWave <Danya.Zharov.2006@gmail.com>
2026-09-16 22:44:08 +0300

    func calc and vector of futures

--- main.cpp
+++ main.cpp
@@ -1,24 +1,50 @@
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <future>
 #include "clicker.hpp"
 
 using data_t = std::vector< unsigned long long >;
 using value_t = data_t::value_type;
+using citer = data_t::const_iterator;
+
+value_t calc(citer begin, citer end)
+{
+  value_t res = 0;
+  for (; begin!=end; ++begin)
+  {
+    res += * begin;
+  }
+  return res;
+}
 
 int main()
 {
+  size_t threads = 1;
   constexpr size_t size{1'000'000'000};
   double init{0}, total{0};
   value_t sum{0};
+  std::vector< std::future< value_t > > results;
+  results.reserve(threads);
+  size_t nums_on_thread = size / threads;
   {
     zharov::Clicker cl;
     data_t values(size, 1);
-    init = cl.millisec();
-    for (size_t i = 0; i < values.size(); ++i)
+    for (size_t i = 0; i < threads - 1; ++i)
     {
-      sum+= values[i];
+      results.push_back(std::async(std::launch::async, calc, values.cbegin() + i * nums_on_thread,
+        values.cbegin() + (i + 1) * nums_on_thread ));
     }
+    results.push_back(std::async(std::launch::async, calc, values.cbegin() + (threads - 1) * nums_on_thread,
+      values.cend()));
+
+    init = cl.millisec();
+
+    for (size_t i = 0; i < threads; ++i)
+    {
+      sum += results[i].get();
+    }
+
     total = cl.millisec();
   }
   std::cout << total - init << "\n";
